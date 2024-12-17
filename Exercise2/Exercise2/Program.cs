952a0de9e4d8dd338d06fafdb7656d84d71b10ff
ABK <abdherz@hotmail.com>
2024-12-17 10:18:44 +0300

    v2

---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.Write("Enter number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("your number is even");
+//}
+//else if (num % 2 == 1)
+//{
+//    Console.WriteLine("your number is odd");
+//}
+
+Console.Write("Enter your Name: ");
+string Name = Console.ReadLine();
+
+Console.Write("Enter Gender: ");
+string Gender = Console.ReadLine();
+
+if (Gender == "male")
+{
+    Console.WriteLine("Hello Mr. " + Name);
+}
+else if (Gender == "female")
+{
+    Console.WriteLine("Hello Ms. " + Name);
+}
+
+Console.WriteLine("Are you active? (yes/no)");
+string Active = Console.ReadLine();
+if (Active == "yes")
+{
+    Console.WriteLine("Access permitted");
+}
+else if (Active == "no")
+{
+    Console.WriteLine("Access denied");
+}
